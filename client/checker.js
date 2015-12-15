5780c7ae1c657095651a996deff9a7abfe464711
rcardiff1 <robertstuartcardiff@gmail.com>
2015-12-14 17:45:44 -0800

    Working Angular application with successful Heroku deployment

---
+++ client/checker.js
@@ -0,0 +1,25 @@
+angular.module('naughtyOrNiceChecker', [])
+
+
+.controller('naughtyOrNiceCtrl', ['$scope', function($scope) {
+	$scope.isNaughtyOrNice = '';
+	$scope.checkerName = '';
+	$scope.imageUrl = '';
+
+	$scope.checkNaughtyOrNice = function(user) {
+		
+		var randomNum = Math.floor(Math.random() * (10 - 1)) + 1;
+
+		if(randomNum > 5) {
+			$scope.isNaughtyOrNice = 'Looks like ' + user + ' has been very nice this year!';
+			$scope.imageUrl = "./Christmas-Gift.gif";
+		} else {
+			$scope.isNaughtyOrNice = 'Looks like ' + user + ' has been very naughty this year!';
+			$scope.imageUrl = "./santa-flipping-the-bird.gif";
+		}
+		$scope.checkerName = user;
+	};
+
+
+
+}]);
--- server/server.js
+++ server/server.js
@@ -2,5 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('client'));
+app.use(express.static( __dirname + '/../client/stylesheet'));
+
 
 module.exports = app;
+
